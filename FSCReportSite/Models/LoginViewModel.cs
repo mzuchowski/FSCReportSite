328f9438834114a875c9fa9952d26a5c81d1dfab
Michał Zuchowski <michalzuch26@gmail.com>
2020-01-30 23:50:14 +0100

    Connect to DB, Create user table

---
+++ FSCReportSite/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSCReportSite.Models
+{
+    public class LoginViewModel
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagane")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; }
+
+        [Compare("Password", ErrorMessage = "Potwierdź hasło")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
