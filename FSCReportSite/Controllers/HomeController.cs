7463a1412ba2eb54f05f2b33aafbb60722220c79
Michał Zuchowski <michalzuch26@gmail.com>
2020-02-07 00:57:28 +0100

    Changes in _Layout, Added views, appearance fixes

--- FSCReportSite/Controllers/HomeController.cs
+++ FSCReportSite/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace FSCReportSite.Controllers
             return View();
         }
 
+        public ViewResult CustomerOrdersReport()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
