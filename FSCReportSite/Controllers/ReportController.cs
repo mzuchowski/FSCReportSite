6b52f5b4d34d74116ae961a42ab20c3f6dc1969f
Michał Zuchowski <michalzuch26@gmail.com>
2020-03-15 15:34:26 +0100

    Moved functions from HomeController to Report model

---
+++ FSCReportSite/Controllers/ReportController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FSCReportSite.Data;
+using FSCReportSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSCReportSite.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ViewResult SolidBoardReportCW()
+        {
+            return View();
+        }
+
+        public ViewResult SolidBoardReportFSC()
+        {
+            return View();
+        }
+        public ViewResult CorrugatedBoardReportCW()
+        {
+            return View();
+        }
+
+        public ViewResult CorrugatedBoardReportFSC()
+        {
+            return View();
+        }
+    }
+}
